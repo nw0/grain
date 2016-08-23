b15ef0f81615ca08a93c01e4f01edd4dbb6aef4f
Nicholas Sim <nicholassimws@gmail.com>
2016-08-23 10:10:54 +0800

    models: Meal, Dish, Ticket: create

--- admin.py
+++ admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import IngredientCategory, Product, Unit, UserProfile
+from .models import (Dish, Ingredient, IngredientCategory, Meal, Product,
+                     Ticket, Unit, UserProfile)
 
 admin.site.register(UserProfile)
 admin.site.register(Unit)
 admin.site.register(IngredientCategory)
 admin.site.register(Product)
+admin.site.register(Meal)
+admin.site.register(Dish)
+admin.site.register(Ingredient)
+admin.site.register(Ticket)
--- models.py
+++ models.py
@@ -51,6 +51,7 @@ class Product(models.Model):
     price = MoneyField(max_digits=10, decimal_places=2, default_currency='GBP')
     amount = models.FloatField()
     units = models.ForeignKey(Unit)
+    fixed = models.BooleanField(default=True)
 
     @python_2_unicode_compatible
     def __str__(self):
@@ -68,6 +69,7 @@ class Ingredient(models.Model):
     owner = models.ForeignKey(UserProfile)
     product = models.ForeignKey(Product)
     price = MoneyField(max_digits=10, decimal_places=2, default_currency='GBP')
+    amount = models.FloatField()
     used_amount = models.FloatField(default=0)
     best_before = models.DateField(blank=True)
     expiry_type = models.CharField(max_length=3, choices=EXP_CHOICES)
@@ -79,7 +81,65 @@ class Ingredient(models.Model):
         return "%s" % self.product
 
 
-# TODO: class Ticket
-# TODO: class Dish
-# TODO: class Meal
+class Meal(models.Model):
+    # FIXME: include docstring
+    BREAKFAST, LUNCH, DINNER, SUPPER, TEA, SNACK = 0, 1, 2, 3, 4, 5
+    MEAL_CHOICES = (
+        (BREAKFAST, "Breakfast"),
+        (LUNCH,     "Lunch"),
+        (DINNER,    "Dinner"),
+        (SUPPER,    "Supper"),
+        (TEA,       "Tea"),
+        (SNACK,     "Snack"),
+    )
+    owner = models.ForeignKey(UserProfile)
+    time = models.DateTimeField()
+    meal_type = models.IntegerField(choices=MEAL_CHOICES)
+    cost_closed = MoneyField(max_digits=10, decimal_places=4)
+    cost_open = MoneyField(max_digits=10, decimal_places=4)
+
+    @python_2_unicode_compatible
+    def __str__(self):
+        return "%s on %s" % (self.get_meal_type_display(), self.time)
+
+
+class Dish(models.Model):
+    # FIXME: include docstring
+    COOKING_STYLES = (
+        ('frying',      "Fried"),
+        ('boiling',     "Boiled"),
+        ('baking',      "Baked"),
+        ('roasting',    "Roasted"),
+        ('uncooked',    "Uncooked"),
+        ('instant',     "Microwaved")
+    )
+    method = models.CharField(max_length=8, choices=COOKING_STYLES)
+    meal = models.ForeignKey(Meal)
+    cost_closed = MoneyField(max_digits=10, decimal_places=4)
+    cost_open = MoneyField(max_digits=10, decimal_places=4)
+
+    class Meta:
+        verbose_name_plural = "dishes"
+
+    @python_2_unicode_compatible
+    def __str__(self):
+        tickets = self.ticket_set.all().order_by('-cost')
+        if tickets.distinct().count() == 0:
+            return "%s (empty)" % self.cooking_style
+        return "%s %s" % (self.get_cooking_style_display(), tickets[0])
+
+
+class Ticket(models.Model):
+    # FIXME: include docstring
+    ingredient = models.ForeignKey(Ingredient)
+    used = models.FloatField()
+    cost = MoneyField(max_digits=10, decimal_places=4)
+    final = models.BooleanField(default=False)
+    dish = models.ForeignKey(Dish)
+
+    @python_2_unicode_compatible
+    def __str__(self):
+        return "%s [%s]" % (self.ingredient, self.used)
+
+
 # TODO: event log
